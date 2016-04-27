b4bb6ea85dcdf832a8d6e7749242504fd1fb871c
Saun Shewanown <saun4app@gmail.com>
2016-04-27 13:54:04 -0400

    Initial unittest

--- dataset/persistence/query.py
+++ dataset/persistence/query.py
@@ -1,13 +1,13 @@
 from collections_extended import setlist
 import sqlize  # sql query builder
-import dataset
 from dataset.persistence.util import get_select_result
+import pprint as pp
 
-
-class SelectBuilder(object):
-    def __init__(self, limit=None, offset=None):
-        self.limit = limit
-        self.offset = offset
+class DatasetSelect(object):
+    def __init__(self, db, limit=None, offset=None, distinct=False):
+        self.select_object = sqlize.Select(limit=limit, offset=offset)
+        self.db = db
+        self.distinct(distinct)
         self._init_param_list_dict()
 
     def _init_param_list_dict(self):
@@ -17,9 +17,13 @@ class SelectBuilder(object):
         for item in item_list:
             self.param_list_dict[item] = setlist()
 
-    def _append_param_item(self, key, value_list):
+    def _append_param_item(self, key, param_item):
         try:
-            self.param_list_dict[key].append(value_list)
+            if isinstance(param_item, list):
+                for value in param_item:
+                    self.param_list_dict[key].append(value)
+            else:
+                self.param_list_dict[key].append(param_item)
         except:
             pass
 
@@ -51,31 +55,44 @@ class SelectBuilder(object):
         self._append_param_item('order_by', column_name)
         return (self)
 
+    def limit(self, value):
+        self.select_object.limit = value
+        return (self)
+
+    def offset(self, value):
+        self.select_object.offset = value
+        return (self)
+
+    def distinct(self, value):
+        self.distinct_value = value
+        return (self)
+
+
     def get_query_string(self):
 
         def get_result_value(value_set_list, default_value=None):
             value_list = list(value_set_list)
             value_list.sort()
             result_value = (default_value, value_list)[len(value_list) > 0]
-
             return (result_value)
 
-        select_object = sqlize.Select(limit=self.limit, offset=self.offset)
-        select_object.sets = get_result_value(self.param_list_dict['table'])
-        select_object.what = get_result_value(self.param_list_dict['column'], ['*'])
-        select_object.where = get_result_value(self.param_list_dict['where'])
-        select_object.where.and_ = get_result_value(self.param_list_dict['where_and'])
-        select_object.where.or_ = get_result_value(self.param_list_dict['where_or'])
-        select_object.group = get_result_value(self.param_list_dict['group_by'])
-        select_object.order = get_result_value(self.param_list_dict['order_by'])
+        def get_query_string():
+            query_string = str(self.select_object)
+            if True == self.distinct_value:
+                query_string = query_string.replace('SELECT ', 'SELECT DISTINCT ')
+            pp.pprint(query_string)
+            return(query_string)
 
-        return (str(select_object))
+        self.select_object.sets = get_result_value(self.param_list_dict['table'])
+        self.select_object.what = get_result_value(self.param_list_dict['column'], ['*'])
+        self.select_object.where = get_result_value(self.param_list_dict['where'])
+        self.select_object.where.and_ = get_result_value(self.param_list_dict['where_and'])
+        self.select_object.where.or_ = get_result_value(self.param_list_dict['where_or'])
+        self.select_object.group = get_result_value(self.param_list_dict['group_by'])
+        self.select_object.order = get_result_value(self.param_list_dict['order_by'])
 
-
-class DatasetSelect(SelectBuilder):
-    def __init__(self, db_string='sqlite:///:memory', limit=None, offset=None):
-        super(self.__class__, self).__init__(limit, offset)
-        self.db = dataset.connect(db_string)
+        q_string = get_query_string()
+        return (q_string)
 
     def select(self, result_type='dict_list'):
         query_string = self.get_query_string()
